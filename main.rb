571624ed6c948a9888674a821ca3e1ff2b292854
cserver45 <77689658+cserver45@users.noreply.github.com>
2023-07-17 16:00:05 -0400

    Inital POC, does get image, now to add other info

--- main.rb
+++ main.rb
@@ -1,17 +1,24 @@
 require 'discordrb'
+require 'httparty'
+require 'nokogiri'
 require 'configatron'
 require_relative 'config.rb'
 
-# 
-bot = Discordrb::Commands::CommandBot.new token: configatron.token, client_id: configatron.clientid, prefix: '$'
+bot = Discordrb::Commands::CommandBot.new token: configatron.token, client_id: configatron.clientid
 
-imgs = "https://www.railpictures.net/photo/"
+uri = "https://www.railpictures.net/photo/"
 
-# to generate random number
-# img_number = rand 1..836514
+# For registering the commands on startup
+bot.register_application_command(:train, 'Get a random train photo.')
 
-bot.message(with_text: "Ping") do |event|
-	event.respond("Pong!")
+
+
+bot.application_command(:train) do |event|
+  img_number = rand 1..836514
+  response = HTTParty.get(uri + img_number.to_s)
+  doc = Nokogiri::HTML(response.body)
+  img = doc.at_xpath('//meta[@property ="og:image"]')['content']
+	event.respond(content: img)
 end
 
 bot.run
