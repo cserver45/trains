4e2e604334ec79cce3b3daef14f92c94670c138e
cserver45 <77689658+cserver45@users.noreply.github.com>
2023-07-18 13:58:39 -0400

    Added desc, check to see if that img exists because of the way I do the randomness

--- main.rb
+++ main.rb
@@ -15,10 +15,25 @@ bot.register_application_command(:train, 'Get a random train photo.')
 
 bot.application_command(:train) do |event|
   img_number = rand 1..836514
+  is_img = false
   response = HTTParty.get(uri + img_number.to_s)
   doc = Nokogiri::HTML(response.body)
-  img = doc.at_xpath('//meta[@property ="og:image"]')['content']
-	event.respond(content: img)
+  while is_img == false do
+    begin
+      img = doc.at_xpath('//meta[@property ="og:image"]')['content']
+      desc = doc.at_xpath('//meta[@name ="description"]')['content']
+    rescue NoMethodError
+      # need to get a new number, else its an infinite loop
+      img_number = rand 1..836514
+      response = HTTParty.get(uri + img_number.to_s)
+      doc = Nokogiri::HTML(response.body)
+	  else
+	    is_img = true
+	  end
+	end
+	
+	event.respond(content: desc)
+	event.send_message(content: img)
 end
 
 bot.run
