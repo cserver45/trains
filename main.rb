ff46fd67f4c080dad1391701c91da22a65091c99
cserver45 <77689658+cserver45@users.noreply.github.com>
2023-07-18 15:25:14 -0400

    Add LICENSE

--- main.rb
+++ main.rb
@@ -1,3 +1,22 @@
+
+=begin
+Trains: A discord bot that scrapes the web to find train photos, and give you details about them in you discord server.
+Copyright (C) 2023 cserver45
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+=end
+
 require 'discordrb'
 require 'httparty'
 require 'nokogiri'
@@ -11,7 +30,9 @@ uri = "https://www.railpictures.net/photo/"
 # For registering the commands on startup
 bot.register_application_command(:train, 'Get a random train photo.')
 
-
+# To show the licence disclaimer
+puts "Copyright (C) 2023 cserver45"
+puts "License details are in main.rb and LICENSE"
 
 bot.application_command(:train) do |event|
   img_number = rand 1..836514
